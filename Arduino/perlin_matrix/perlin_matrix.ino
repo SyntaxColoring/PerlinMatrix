894e0aa57d1f24e8ad3463f52e59e956a879974b
Max Marrone <max@marrone.nyc>
2022-07-28 19:55:11 -0400

    Delete CircuitPython and Arduino versions.

--- Arduino/perlin_matrix/perlin_matrix.ino
+++
@@ -1,1 +0,0 @@
-// https://forum.arduino.cc/t/how-to-avoid-the-quirks-of-the-ide-sketch-file-pre-preprocessing/262594/2
--- Arduino/perlin_matrix/xxh32.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef XXH32_HPP_INCLUDED
-#define XXH32_HPP_INCLUDED
-
-#include <cstdint>
-
-std::uint32_t xxh_32(const uint32_t to_hash, const uint32_t seed);
-
-#endif
